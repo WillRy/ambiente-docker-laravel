9cb2274fd4d126b02a53e265ddff7cc427bd2a90
William R <wiribasczky@gmail.com>
2021-11-28 00:28:05 -0300

    Adicionado versao de modphp
    - Adicionado versao de container do php/apache
      com modphp, porem vem desabilitado por padrao.
    
    - Por padrao, esta habilitado o container do php
      e outro do nginx, para fpm

---
+++ php7-3-6-buster/docker/modphp/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dockerize -wait tcp://db:3306 -timeout 90s
+
+chown -R www-data:www-data .
+
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+if [ ! -f ".env.testing" ]; then
+  cp .env.testing.example .env.testing
+fi
+
+composer install
+
+docker-php-entrypoint 
+
+apache2-foreground
